e63d068036088dce9475bf2d87bd2ef2beb61975
nobodyczcz <jianshen858@gmail.com>
2019-08-08 12:59:19 +1000

    Add project files.

---
+++ CBSH-rect-cmake/CBSH-rect-cmake.cpp
@@ -0,0 +1,12 @@
+﻿// CBSH-rect-cmake.cpp : Defines the entry point for the application.
+//
+
+#include "CBSH-rect-cmake.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
