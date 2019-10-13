87b7f16d631c81ac4f7808919d3dbbbe02d960a2
nobodyczcz <jianshen858@gmail.com>
2019-10-14 00:29:20 +1100

    bug fix

--- CBSH-rect-cmake/agents_loader.cpp
+++ CBSH-rect-cmake/agents_loader.cpp
@@ -26,13 +26,13 @@ AgentsLoader::AgentsLoader(p::object agents) {
 	for (int i = 0; i < num_of_agents; i++) {
 		pair<int, int> initial;
 		pair<int, int> goal;
-		p::tuple iniTuple(agents[i].attr("initial_position"));
-		initial.first = p::extract<int>(iniTuple[0]);
-		initial.second = p::extract<int>(iniTuple[1]);
+		p::tuple iniTuple(agents[i].attr("position"));
+		initial.first = p::extract<int>(p::long_(iniTuple[0]));
+		initial.second = p::extract<int>(p::long_(iniTuple[1]));
 		p::tuple goalTuple(agents[i].attr("target"));
-		goal.first = p::extract<int>(goalTuple[0]);
-		goal.second = p::extract<int>(goalTuple[1]);
-		int heading = p::extract<int>(agents[i].attr("direction"));
+		goal.first = p::extract<int>(p::long_(goalTuple[0]));
+		goal.second = p::extract<int>(p::long_(goalTuple[1]));
+		int heading = p::extract<int>(p::long_(agents[i].attr("direction")));
 		this->initial_locations.push_back(initial);
 		this->goal_locations.push_back(goal);
 		this->headings.push_back(heading);
