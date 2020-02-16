feff9e57e2f5d5ee788283944edd7a37d64f6109
nobodyczcz <jianshen858@gmail.com>
2020-02-16 12:11:12 +1100

    APPLE support

--- CBSH-rect-cmake/Conflict.h
+++ CBSH-rect-cmake/Conflict.h
@@ -354,7 +354,7 @@ public:
 		int a2Rg = getMahattanDistance(s2_x, s2_y, Rg_x, Rg_y);
 		int a2RgBypass = a2Rg + 2 * (getMahattanDistance(s1_x, s1_y, Rs.first, Rs.second) + 1);
 
-		if (RM4way ==0) {
+		if (RM4way ==1) {
 			a2_4way = a2RgBypass <= a2Rg + k;
 			a1_4way = a1RgBypass <= a1Rg + k;
 		}
